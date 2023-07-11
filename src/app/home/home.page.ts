8e083c09e5ea96ead467a07f9a1cef94ebcecdbb
Luis <yan.tao.he2881@gmail.com>
2023-07-11 16:39:49 -0400

    feat: built ion-date app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public date: string = '';
+  public form: FormGroup;
+  public minDate: string = '';
+  public maxDate: string = '';
+  public dateToday: string = '';
+  public dateYesterday: string = '';
+  public tenDays: string = '';
+  public nextYear: string = '';
+  public dateValue: any = '';
+  public showList: boolean = false;
 
-  constructor() {}
+  constructor(private fb: FormBuilder) {
+    this.form = this.createForm();
+    this.minDate = new Date().toISOString();
+    this.maxDate = (new Date().getFullYear() + 5).toString();
+  }
 
+  createForm(): FormGroup {
+    return this.fb.group({
+      date: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    this.showList = true;
+    this.dateToday = this.minDate;
+    this.dateYesterday = this.getNewDate(1, true);
+    this.tenDays = this.getNewDate(10, false);
+    this.nextYear = this.getNewDate(366, false);
+  }
+
+  getNewDate(days: number, previous: boolean) {
+    let d = new Date();
+    if (previous) {
+      d.setDate(d.getDate() - days);
+    } else {
+      d.setDate(d.getDate() + days);
+    }
+    return new Date(d).toISOString();
+  }
 }
